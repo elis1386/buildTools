827b0c2cb7256c71bfc9aae40f0c07284d4a3136
Elizaveta <elis1386@gmail.com>
2021-09-02 10:41:23 +0300

    added lesson1

---
+++ lesson1/js/main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const time = document.querySelector('.stopwatch')
+const mainButton = document.querySelector('#main-button')
+const clearButton = document.querySelector('#clear-button')
+const stopwatch = { elapsedTime: 0 }
+
+import { displayTime } from './displayTime.js';
+import { startStopwatch } from './stopwatch.js';
+
+// Adding Event Listiners to buttons
+
+mainButton.addEventListener('click', () => {
+    if (mainButton.innerHTML === 'Start') {
+        startStopwatch();
+        mainButton.innerHTML = 'Stop'
+    } else {
+        stopwatch.elapsedTime += Date.now() - stopwatch.startTime
+        clearInterval(stopwatch.intervalId)
+        mainButton.innerHTML = 'Start'
+    }
+})
+
+clearButton.addEventListener('click', () => {
+    stopwatch.elapsedTime = 0
+    stopwatch.startTime = Date.now()
+    displayTime(0, 0, 0, 0)
+})
+
+
+
+
+
+
+
