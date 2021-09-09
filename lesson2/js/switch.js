67d4aad7f42a97b8c1d38079c03f350d3d901472
Elizaveta <elis1386@gmail.com>
2021-09-09 10:48:57 +0300

    lesson2 parcel

--- lesson2/js/switch.js
+++ lesson2/js/switch.js
@@ -1,15 +1,15 @@
 import { clearParagraph, calcHTML, timerHTML } from "./output.js"
 import { handleForm, diff } from "./calcDateDiff.js";
 import listener from "./timer.js"
-import bundlerPic from "../bundler.png"
+// import bundlerPic from "../bundler.png"
 
-const pic = document.createElement('img')
+// const pic = document.createElement('img')
 
-pic.src = bundlerPic
+// pic.src = bundlerPic
 
-pic.onload = () => {
-    document.body.appendChild(pic)
-}
+// pic.onload = () => {
+//     document.body.appendChild(pic)
+// }
 
 document.querySelectorAll('.switchBtn').forEach(btn => {
     const outputBlock = document.querySelector('.service');
